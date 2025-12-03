da067c477d4c3ab0d83b8c6c0196f91cf4008613
ZiadHF <ziadhfahmy@yahoo.com>
2025-12-03 02:37:07 +0200

    pset 3

---
+++ psets/Problem Set 3/options.py
@@ -0,0 +1,43 @@
+# This file contains the options that you should modify to solve Question 2
+
+def question2_1():
+    return {
+        "noise": 0,
+        "discount_factor": 0.1,
+        "living_reward": 0
+    }
+
+def question2_2():
+    return {
+        "noise": 0.1,
+        "discount_factor": 0.1,
+        "living_reward": -0.1
+    }
+
+def question2_3():
+    return {
+        "noise": 0,
+        "discount_factor": 1,
+        "living_reward": -0.1
+    }
+
+def question2_4():
+        return {
+        "noise": 0.2,
+        "discount_factor": 1,
+        "living_reward": -0.1
+    }
+
+def question2_5():
+    return {
+        "noise": 0,
+        "discount_factor": 1,
+        "living_reward": 10
+    }
+
+def question2_6():
+    return {
+        "noise": 0,
+        "discount_factor": 1,
+        "living_reward": -100
+    }
---
+++ psets/Problem Set 3/value_iteration.py
@@ -0,0 +1,89 @@
+from typing import Dict, Optional
+from agents import Agent
+from environment import Environment
+from mdp import MarkovDecisionProcess, S, A
+import json
+from helpers.utils import NotImplemented
+
+# This is a class for a generic Value Iteration agent
+class ValueIterationAgent(Agent[S, A]):
+    mdp: MarkovDecisionProcess[S, A] # The MDP used by this agent for training 
+    utilities: Dict[S, float] # The computed utilities
+                                # The key is the string representation of the state and the value is the utility
+    discount_factor: float # The discount factor (gamma)
+
+    def __init__(self, mdp: MarkovDecisionProcess[S, A], discount_factor: float = 0.99) -> None:
+        super().__init__()
+        self.mdp = mdp
+        self.utilities = {state:0 for state in self.mdp.get_states()} # We initialize all the utilities to be 0
+        self.discount_factor = discount_factor
+    
+    # Given a state, compute its utility using the bellman equation
+    # if the state is terminal, return 0
+    def compute_bellman(self, state: S) -> float:
+        if self.mdp.is_terminal(state):
+            return 0.0
+        curr_bellman = float('-INF')
+        for action in self.mdp.get_actions(state):
+            bellman = 0.0
+            next_states = self.mdp.get_successor(state, action)
+            for s, p in next_states.items():
+                r = self.mdp.get_reward(state, action , s)
+                bellman += p * (r + self.discount_factor * self.utilities[s])
+            curr_bellman = max(curr_bellman, bellman)
+        return curr_bellman
+                   
+    
+    # Applies a single utility update
+    # then returns True if the utilities has converged (the maximum utility change is less or equal the tolerance)
+    # and False otherwise
+    def update(self, tolerance: float = 0) -> bool:
+        V = self.utilities.copy()
+        max_diff = 0.0
+        for state in self.mdp.get_states():
+            new_utility = self.compute_bellman(state)
+            max_diff = max(max_diff, abs(new_utility - self.utilities[state]))
+            V[state] = new_utility
+        self.utilities = V
+        return max_diff <= tolerance
+
+    # This function applies value iteration starting from the current utilities stored in the agent and stores the new utilities in the agent
+    # NOTE: this function does incremental update and does not clear the utilities to 0 before running
+    # In other words, calling train(M) followed by train(N) is equivalent to just calling train(N+M)
+    def train(self, iterations: Optional[int] = None, tolerance: float = 0) -> int:
+        it = 0
+        while True:
+            it += 1
+            converged = self.update(tolerance)
+            if converged or (iterations is not None and it >= iterations):
+                break
+        return it
+    
+    # Given an environment and a state, return the best action as guided by the learned utilities and the MDP
+    # If the state is terminal, return None
+    def act(self, env: Environment[S, A], state: S) -> A:
+        if self.mdp.is_terminal(state):
+            return None
+        best_action = None
+        for action in self.mdp.get_actions(state):
+            next_states = self.mdp.get_successor(state, action)
+            expected_utility = 0.0
+            for s, p in next_states.items():
+                r = self.mdp.get_reward(state, action , s)
+                expected_utility += p * (r + self.discount_factor * self.utilities[s])
+            if best_action is None or expected_utility > best_expected_utility:
+                best_action = action
+                best_expected_utility = expected_utility
+        return best_action
+    
+    # Save the utilities to a json file
+    def save(self, env: Environment[S, A], file_path: str):
+        with open(file_path, 'w') as f:
+            utilities = {self.mdp.format_state(state): value for state, value in self.utilities.items()}
+            json.dump(utilities, f, indent=2, sort_keys=True)
+    
+    # loads the utilities from a json file
+    def load(self, env: Environment[S, A], file_path: str):
+        with open(file_path, 'r') as f:
+            utilities = json.load(f)
+            self.utilities = {self.mdp.parse_state(state): value for state, value in utilities.items()}
